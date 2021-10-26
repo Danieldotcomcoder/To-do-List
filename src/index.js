2174c08e63eb342d1ee13357db928458e119964d
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-10-26 18:24:17 +0300

    Add style image

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import _ from 'lodash';
 import './style.css';
 import menu from './menu.png';
 import reload from './reload.png';
+import enter from './to-left.png';
 
 const relaodimg = new Image();
 relaodimg.src = reload;
 const test = new _();
 test.src = _;
+const entersign = new Image();
+entersign.src = enter;
 
 document.querySelector('.top-list').appendChild(relaodimg);
+document.querySelector('.list-input').appendChild(entersign);
 
 const arraylists = [
   {
@@ -38,7 +42,6 @@ arraylists.sort((a, b) => (a.index - b.index)); // Sort the arraylist by index n
 arraylists.forEach((item, i) => { // Display elements on screen by index number from small to big.
   const threedots = new Image();
   threedots.src = menu;
-
   const list = document.querySelector('.list-elements');
   const itemlist = document.createElement('li');
   itemlist.innerHTML = `<input type='checkbox' class='check-box'>
