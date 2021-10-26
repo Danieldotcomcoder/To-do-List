81015517a822903bf4ff69b17ee285db92a3f945
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-10-26 16:56:34 +0300

    Add main structure

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # To-Do-List
+# To-Do-List Project.
+
+> This project is built with HTML, CSS and JavaScript.
+
+> Implemented Functions by using JavaScript to add and remove lists.
+
+## Built With
+### Major languages
+- HTML
+- CSS
+- Javascript
+
+### Technologies used
+- Github
+- Git
+- Visual Studio Code
+
+
+## Getting Started
+To get a local copy up and running follow these simple example steps.
+git clone   https://github.com/Danieldotcomcoder/To-Do-List
+
+
+## Author
+👤 Daniel Shdeed
+
+- GitHub: [https://github.com/Danieldotcomcoder)]
+- Twitter: [https://twitter.com/DannyDotcoder)]
+- LinkedIn: [https://www.linkedin.com/in/daniel-shdeed-832b03115/]
+
+## 🤝 Contributing
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the issues page.
+
+
+## Show your support
+Give a ⭐️ if you like this project!
+
+📝 License
+This project is MIT licensed.
--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
 import _ from 'lodash';
 import './style.css';
+import menu from './menu.png';
+import reload from './reload.png';
+
+const relaodimg = new Image();
+relaodimg.src = reload;
+const test = new _();
+test.src = _;
+
+document.querySelector('.top-list').appendChild(relaodimg);
+
+const arraylists = [
+  {
+    description: 'Go to the grocery store',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'study for 2 hours',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'go to the gym',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'read a book',
+    completed: false,
+    index: 4,
+  },
+];
+
+arraylists.sort((a, b) => (a.index - b.index)); // Sort the arraylist by index number.
+
+arraylists.forEach((item, i) => { // Display elements on screen by index number from small to big.
+  const threedots = new Image();
+  threedots.src = menu;
+
+  const list = document.querySelector('.list-elements');
+  const itemlist = document.createElement('li');
+  itemlist.innerHTML = `<input type='checkbox' class='check-box'>
+  <h3>${arraylists[i].description}<h3>`;
+  itemlist.appendChild(threedots);
+  list.appendChild(itemlist);
+});
